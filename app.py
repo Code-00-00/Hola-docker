f83a4d051a88a195a1d1af22d7585fb37f16b8e3
hmmm <vargasbrandon076@gmail.com>
2025-09-26 07:24:49 -0500

    Primer commit: subiendo proyecto completo

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///flaskdb.sqlite'
+    
+# Obtener el valor de la variable de entorno, si no existe, usar un valor por defecto
+app_name = os.getenv('APP_NAME', 'Aplicación Flask')
+
+@app.route('/')
+def hola_mundo():
+    return f'¡Hola Mundo con Flask desde {app_name}!'
+
+@app.route('/index')
+def hola_adso():
+    return f'¡Hola - ADSO desde {app_name}!'
+
+@app.route('/index2')
+def hola_adso2():
+    return f'¡Hola - ADSO2 desde {app_name}!'
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=5000)
